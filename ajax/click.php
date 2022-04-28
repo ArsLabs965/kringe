edb8414859bcc714dd31928e48ba24f938486baf
ArsLabs965 <arsenov965@gmail.com>
2022-04-28 12:57:12 +0300

    Добавлен уровень кнопки

--- ajax/click.php
+++ ajax/click.php
@@ -5,7 +5,7 @@
     $logge = mysqli_query($connection, "SELECT * FROM `accaunts` WHERE `login` = '$_SESSION[user]'");
     if(($ac = mysqli_fetch_assoc($logge))){
         $coins = $ac['coins'];
-        $coins++;
+        $coins += $ac['level'];
         mysqli_query($connection, "UPDATE `accaunts` SET `coins` = '$coins' WHERE `login` = '$_SESSION[user]'");
         echo $coins;
     }
--- coin.php
+++ coin.php
@@ -10,7 +10,22 @@ if($_SESSION['user'] == NULL){
     header("Location: http://" . $_SERVER['SERVER_NAME'] . "/" . $rootway . "index.php");
     exit();
 }
-
+if(isset($_POST['lvlup'])){
+   
+    $recv = mysqli_query($connection, "SELECT * FROM `accaunts` WHERE `login` = '$_SESSION[user]'");
+    if(($ac = mysqli_fetch_assoc($recv))){
+        if($ac['coins'] >= $ac['level'] * $ac['level'] * 1000){
+            $level_l = $ac['level'] + 1;
+            $coins_l = $ac['coins'] - $ac['level'] * $ac['level'] * 1000;
+            $forboss = $ac['level'] * $ac['level'] * 1000;
+            $slil = $ac['lost'] + $ac['level'] * $ac['level'] * 1000;
+            mysqli_query($connection, "UPDATE `accaunts` SET `coins` = '$forboss' WHERE `login` = 'BOSS'");
+            mysqli_query($connection, "UPDATE `accaunts` SET `lost` = '$slil' WHERE `login` = '$_SESSION[user]'");
+            mysqli_query($connection, "UPDATE `accaunts` SET `level` = '$level_l' WHERE `login` = '$_SESSION[user]'");
+            mysqli_query($connection, "UPDATE `accaunts` SET `coins` = '$coins_l' WHERE `login` = '$_SESSION[user]'");
+        }
+    }
+}
 
 ?>
 <!DOCTYPE html>
@@ -33,6 +48,11 @@ if($_SESSION['user'] == NULL){
         ?><a class="grey"> LCN</a></h1><br><br>
 
         <img onclick="cl()" src="img/btn.png" width="200px" alt=""><br><br>
+            Ваш уровень - <?php echo $ac['level']; ?><br><br>
+                <div id="lvl">
+                    
+                </div>
+        <br><br>
         <div class="form">
             <h3>Рулетка</h3>
             <a class="grey">Будет отниматься 5% от каждой выигранной суммы</a><br>
@@ -48,6 +68,7 @@ if($_SESSION['user'] == NULL){
     <script src="js/jq.js"></script>
     <script>
         var coins = <?php echo $ac['coins']; ?>;
+        var level = <?php echo $ac['level']; ?>;
         function cl(){
             $.ajax({
       method: "GET",
@@ -63,6 +84,11 @@ if($_SESSION['user'] == NULL){
         }
 
         function loop(){
+            if(coins >= level * level * 1000){
+                $("#lvl").html('<form action="" method="POST"><input class="green" type="submit" value="Повысить уровень" name="lvlup"></form>');
+            }else{
+                $("#lvl").html("Вам нужно " + level * level * 1000 + " LCN чтобы повысить уровень");
+            }
             if($("#chance").val() != '' && $("#stavka").val() != ''){
                 if($("#chance").val() < 2 || $("#chance").val() > 94 || $("#stavka").val() < 100 || $("#stavka").val() > coins){
                     $("#play_btn").html("Недопустимые значения!");
